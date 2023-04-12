b4eaebbc1364d93c7af09ec38483aae439fd3e84
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-04-12 18:49:59 -0500

    Aplico correctamente la estructura del constructor de la clase StringUtilTest y indicamos la notacion @Test para este metodo

--- src/test/java/com/testing/javatests/util/StringUtilTest.java
+++ src/test/java/com/testing/javatests/util/StringUtilTest.java
@@ -3,13 +3,16 @@ package com.testing.javatests.util;
 // import static org.junit.Assert.*;
 
 import org.junit.Assert;
+import org.junit.Test;
 import org.omg.SendingContext.RunTime;
 
 
 public class StringUtilTest {
 
     //tavulador de main: psvm
-    public static void main(String[] args) {
+
+    @Test
+    public void testRepeat() {
 
         Assert.assertEquals("HolaHolaHola" , StringUtil.repeat("Hola", 3));
         Assert.assertEquals("Hola" , StringUtil.repeat("Hola2", 1));
