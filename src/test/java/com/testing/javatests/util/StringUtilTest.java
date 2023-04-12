4b0fd2f77cb23ba6f6592398ec29586a988d7e9b
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-04-12 18:46:18 -0500

    Generamos el error nuevamente y implementadmos la clase Asser de la libreria JUnit que hace lo mismo que el codigo que implementamos anterior mente para las pruebas, ya que esta es una librerira espcialida para pruebas

--- src/test/java/com/testing/javatests/util/StringUtilTest.java
+++ src/test/java/com/testing/javatests/util/StringUtilTest.java
@@ -2,22 +2,19 @@ package com.testing.javatests.util;
 
 // import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.omg.SendingContext.RunTime;
+
 
 public class StringUtilTest {
 
     //tavulador de main: psvm
     public static void main(String[] args) {
 
-        assertEquals(StringUtil.repeat("Hola", 3),"HolaHolaHola");
+        Assert.assertEquals("HolaHolaHola" , StringUtil.repeat("Hola", 3));
+        Assert.assertEquals("Hola" , StringUtil.repeat("Hola2", 1));
 
     }
 
-    private static void assertEquals(String actual, String expected) {
 
-        if (!actual.equals(expected)) {
-            //System.out.println("Error");
-            throw new RuntimeException(actual + "is not equal to " + expected);
-        }
-    }
 }
