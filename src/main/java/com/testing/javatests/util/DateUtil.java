ddbcbe7f39b116e5ca4d38a1ce3b4d1cea8bf5f6
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-04-14 19:16:17 -0500

    Aplico simplificacion al if del metodo isLeapYear de la clase DateUtil

--- src/main/java/com/testing/javatests/util/DateUtil.java
+++ src/main/java/com/testing/javatests/util/DateUtil.java
@@ -7,11 +7,7 @@ public class DateUtil {
     public static boolean isLeapYear(int year) {
 
 
-        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) ) {
-            return true;
-        } else {
-            return false;
-        }
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
 
 
     }
